d2d92122ae6e7401fcdbef38cda8ce3869245a61
Arturo Gil <charturo.gil@gmail.com>
2024-05-29 09:34:53 -0600

    Binary files changes

--- src/main/java/BinaryFiles.java
+++ src/main/java/BinaryFiles.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class BinaryFiles {
     public static void main(String[] args) {
@@ -8,19 +9,26 @@ public class BinaryFiles {
             FileInputStream fis = new FileInputStream("numbers.seq");
             DataInputStream dis = new DataInputStream(fis);
         ) {
-            for (int i = 0; i < 512; i++) {
-                dos.writeInt(i);
-            }
 
             while (dis.available() > 0) {
-                int integer = dis.readInt();
-                System.out.println("NUMBER: " + integer);
+                dis.readByte();
             }
-
         }
          catch (IOException e) {
             System.out.println("ERROR OPENING FILE");
             e.printStackTrace();
         }
     }
+//    public static void main(String[] args) {
+//        String str = "Hola Mundo";
+//        byte[] byteArray = str.getBytes();
+//        System.out.println(byteArray.length);
+//
+//        for(byte b : byteArray) {
+//            System.out.println("BYTE: "+ b);
+//        }
+//
+//        String newString = new String(byteArray, StandardCharsets.UTF_8);
+//        System.out.println("newString: " + newString);
+//    }
 }
